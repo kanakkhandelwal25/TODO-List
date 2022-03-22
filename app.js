1f91070b6bd6b243fa480bf7824bd586fbb5439e
kanakkhandelwal25 <68547212+kanakkhandelwal25@users.noreply.github.com>
2022-03-22 20:32:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//Event Listeners
+todoButton.addEventListener('click',addTodo);
+
+//Functions
+
+function addTodo(event){
+    //Prevent form from submitting
+    event.preventDefault();
+    
+    //TODO div
+
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //Check Mark Button
+    const completeButton = document.createElement('button');
+    completeButton.innerHTML = '<i class="fas fa-check"></i>';
+    completeButton.classList.add('complete-btn');
+    todoDiv.appendChild(completeButton);
+    //Delete Button
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+    deleteButton.classList.add('delete-btn');
+    todoDiv.appendChild(deleteButton);
+    //Append to List
+    todoList.appendChild(todoDiv);
+    //Clear TODO input value
+    todoInput.value = '';
+}
